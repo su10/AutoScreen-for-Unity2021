e1bf9721f5fec89c01b5b7e7248c0be837b90fea
su10 <1687366+su10@users.noreply.github.com>
2019-10-22 15:36:11 +0900

    GameViewSizeクラス追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/GameViewSize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Jagapippi.AutoScreen
+{
+    public enum GameViewSizeType
+    {
+        AspectRatio,
+        FixedResolution
+    }
+
+    [Serializable]
+    public class GameViewSize
+    {
+        [SerializeField] private string m_BaseText;
+        [SerializeField] private GameViewSizeType m_SizeType;
+        [SerializeField] private int m_Width;
+        [SerializeField] private int m_Height;
+
+        public string baseText => m_BaseText;
+        public GameViewSizeType sizeType => m_SizeType;
+        public int width => m_Width;
+        public int height => m_Height;
+
+        public GameViewSize(string baseText, GameViewSizeType sizeType, int width, int height)
+        {
+            m_BaseText = baseText;
+            m_SizeType = sizeType;
+            m_Width = width;
+            m_Height = height;
+        }
+    }
+}
