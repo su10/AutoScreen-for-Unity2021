0d621c39efd082564e8888f8ca3a4513e4445e95
su10 <1687366+su10@users.noreply.github.com>
2019-10-22 15:38:46 +0900

    GameViewProxyクラス追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/GameViewProxy.cs
@@ -0,0 +1,41 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jagapippi.AutoScreen
+{
+    public static class GameViewProxy
+    {
+        private const BindingFlags InstanceFlag = BindingFlags.Instance | BindingFlags.NonPublic;
+        private const BindingFlags StaticFlag = BindingFlags.Static | BindingFlags.NonPublic;
+
+        private static readonly Type GameView = Assembly.Load("UnityEditor.dll").GetType("UnityEditor.GameView");
+        private static readonly PropertyInfo HasFocus = GameView.GetProperty("hasFocus", InstanceFlag);
+        private static readonly PropertyInfo CurrentSizeGroupType = GameView.GetProperty("currentSizeGroupType", StaticFlag);
+        private static readonly PropertyInfo SelectedSizeIndex = GameView.GetProperty("selectedSizeIndex", InstanceFlag);
+        private static readonly PropertyInfo CurrentGameViewSize = GameView.GetProperty("currentGameViewSize", InstanceFlag);
+
+        private static EditorWindow instance => EditorWindow.GetWindow(GameView, false, "Game", false);
+        public static bool isOpen => (0 < Resources.FindObjectsOfTypeAll(GameView).Length);
+        public static bool hasFocus => (isOpen && (bool) HasFocus.GetValue(instance, null));
+        public static GameViewSizeGroupType currentSizeGroupType => (GameViewSizeGroupType) CurrentSizeGroupType.GetValue(instance, null);
+        public static int selectedSizeIndex => (int) SelectedSizeIndex.GetValue(instance, null);
+
+        public static GameViewSize currentGameViewSize
+        {
+            get
+            {
+                if (isOpen == false) return null;
+
+                var gameViewSize = CurrentGameViewSize.GetValue(instance, null);
+                var json = JsonUtility.ToJson(gameViewSize);
+                return JsonUtility.FromJson<GameViewSize>(json);
+            }
+        }
+
+        public static void Repaint() => instance.Repaint();
+    }
+}
+#endif
