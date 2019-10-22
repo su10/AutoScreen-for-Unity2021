00aee08e81d4a460bc50b9c9411960b4ff82968d
su10 <1687366+su10@users.noreply.github.com>
2019-10-22 17:51:16 +0900

    GameViewを監視してイベントを発行するクラス追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/GameViewEvent.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+
+namespace Jagapippi.AutoScreen
+{
+    public static class GameViewEvent
+    {
+        public static event Action<GameViewSize> resolutionChanged;
+
+        [InitializeOnLoadMethod]
+        static void Initialize()
+        {
+            var previousSizeIndex = -1;
+
+            EditorApplication.update += () =>
+            {
+                if (GameViewProxy.hasFocus == false) return;
+
+                var currentSizeIndex = GameViewProxy.selectedSizeIndex;
+                if (previousSizeIndex == currentSizeIndex) return;
+
+                resolutionChanged?.Invoke(GameViewProxy.currentGameViewSize);
+                previousSizeIndex = currentSizeIndex;
+            };
+        }
+    }
+}
+#endif
