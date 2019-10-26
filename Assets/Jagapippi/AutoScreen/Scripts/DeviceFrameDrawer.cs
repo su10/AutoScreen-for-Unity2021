1e8da8061c7d3ca388e3aff513c7fee54cb2778a
su10 <1687366+su10@users.noreply.github.com>
2019-10-26 17:03:19 +0900

    GameViewにデバイスのフレーム画像を表示するコンポーネント追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/DeviceFrameDrawer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.AutoScreen
+{
+    [ExecuteAlways]
+    public class DeviceFrameDrawer : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private Sprite _frame;
+
+        void OnEnable()
+        {
+            CurrentGameViewScreen.changed += screen => _frame = screen?.frame;
+            _frame = CurrentGameViewScreen.value?.frame;
+        }
+
+        void OnGUI()
+        {
+            if (_frame == null) return;
+
+            GUI.DrawTexture(
+                position: _frame.textureRect,
+                image: _frame.texture,
+                scaleMode: ScaleMode.StretchToFill,
+                alphaBlend: true,
+                imageAspect: 0,
+                color: Color.black,
+                borderWidth: 0,
+                borderRadius: 0
+            );
+        }
+#endif
+    }
+}
