ea21b32a68af4762d37dfca048a21ff615b6862b
su10 <1687366+su10@users.noreply.github.com>
2019-10-26 16:15:22 +0900

    GameViewで選択している解像度に応じてGameViewScreenを返すクラス追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/CurrentGameViewScreen.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jagapippi.AutoScreen
+{
+    public class CurrentGameViewScreen : ScriptableSingleton<CurrentGameViewScreen>
+    {
+        [SerializeField] private GameViewScreenAsset _asset;
+
+        public static GameViewScreen value => instance._asset?.data;
+        public static event Action<GameViewScreen> changed;
+
+        [InitializeOnLoadMethod]
+        static void Initialize()
+        {
+            GameViewEvent.resolutionChanged += size => instance.Load(size);
+        }
+
+        private void Load(GameViewSize size)
+        {
+            var previous = _asset;
+            var current = GameViewScreenAsset.Load(size.baseText);
+
+            if (previous == null && current == null) return;
+            if (previous != null && previous.Equals(current)) return;
+
+            _asset = current;
+            changed?.Invoke(_asset?.data);
+        }
+    }
+}
+#endif
