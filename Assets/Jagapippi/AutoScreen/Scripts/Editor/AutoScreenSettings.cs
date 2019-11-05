e1932992519dba3f7f567e02be1b1e414308aedc
su10 <1687366+su10@users.noreply.github.com>
2019-11-06 01:36:45 +0900

    エディター上の設定を読み書きするクラス追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/AutoScreenSettings.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Jagapippi.AutoScreen
+{
+    public static class AutoScreenSettings
+    {
+        public static readonly DeviceFrame deviceFrame = new DeviceFrame();
+
+        public class DeviceFrame
+        {
+            private readonly string EnabledKey = string.Join(".", typeof(DeviceFrame).FullName, nameof(enabled));
+
+            public bool enabled
+            {
+                get { return EditorPrefs.GetBool(EnabledKey, true); }
+                set { EditorPrefs.SetBool(EnabledKey, value); }
+            }
+        }
+
+        public static readonly SafeAreaBorder safeAreaBorder = new SafeAreaBorder();
+
+        public class SafeAreaBorder
+        {
+            private readonly string EnabledKey = string.Join(".", typeof(SafeAreaBorder).FullName, nameof(enabled));
+
+            public bool enabled
+            {
+                get { return EditorPrefs.GetBool(EnabledKey, true); }
+                set { EditorPrefs.SetBool(EnabledKey, value); }
+            }
+        }
+    }
+}
+#endif
