a2066c8f1d58154594af63494f0c6156cf0d2d74
su10 <1687366+su10@users.noreply.github.com>
2019-11-09 20:10:25 +0900

    プレハブのヘルパークラス追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/PrefabHelper.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+
+namespace Jagapippi.AutoScreen
+{
+    public static class PrefabHelper
+    {
+        public static bool IsInHierarchy(Component component)
+        {
+            return (IsInPrefabAsset(component) == false) && (IsInPrefabStage(component) == false);
+        }
+
+        public static bool IsInPrefabAsset(Component component)
+        {
+            return AssetDatabase.Contains(component.transform.root.gameObject);
+        }
+
+        public static bool IsInPrefabStage(Component component)
+        {
+            return PrefabStageUtility.GetCurrentPrefabStage()?.IsPartOfPrefabContents(component.gameObject) ?? false;
+        }
+    }
+}
+#endif
