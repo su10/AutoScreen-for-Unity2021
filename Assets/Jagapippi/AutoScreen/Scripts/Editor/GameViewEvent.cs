de1f9317e32f39a1d22a52f5e84d42dcc2d192fe
su10 <1687366+su10@users.noreply.github.com>
2019-11-09 14:03:47 +0900

    GameViewが開いたときと閉じたときのイベント追加

--- Assets/Jagapippi/AutoScreen/Scripts/Editor/GameViewEvent.cs
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/GameViewEvent.cs
@@ -7,22 +7,47 @@ namespace Jagapippi.AutoScreen
     public static class GameViewEvent
     {
         public static event Action<GameViewSize> resolutionChanged;
+        public static event Action opened;
+        public static event Action closed;
 
         [InitializeOnLoadMethod]
         static void Initialize()
         {
-            var previousSizeIndex = -1;
-
-            EditorApplication.update += () =>
             {
-                if (GameViewProxy.hasFocus == false) return;
+                var previousSizeIndex = -1;
 
-                var currentSizeIndex = GameViewProxy.selectedSizeIndex;
-                if (previousSizeIndex == currentSizeIndex) return;
+                EditorApplication.update += () =>
+                {
+                    if (GameViewProxy.hasFocus == false) return;
 
-                resolutionChanged?.Invoke(GameViewProxy.currentGameViewSize);
-                previousSizeIndex = currentSizeIndex;
-            };
+                    var currentSizeIndex = GameViewProxy.selectedSizeIndex;
+                    if (previousSizeIndex == currentSizeIndex) return;
+
+                    resolutionChanged?.Invoke(GameViewProxy.currentGameViewSize);
+                    previousSizeIndex = currentSizeIndex;
+                };
+            }
+
+            {
+                var isOpen = false;
+
+                EditorApplication.update += () =>
+                {
+                    if (isOpen == false && GameViewProxy.isOpen)
+                    {
+                        opened?.Invoke();
+                        isOpen = true;
+                        return;
+                    }
+
+                    if (isOpen && GameViewProxy.isOpen == false)
+                    {
+                        closed?.Invoke();
+                        isOpen = false;
+                        return;
+                    }
+                };
+            }
         }
     }
 }
