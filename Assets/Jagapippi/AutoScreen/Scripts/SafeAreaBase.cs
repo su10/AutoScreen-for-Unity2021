53f0e4de34612770321dc274848e133b414147f6
su10 <1687366+su10@users.noreply.github.com>
2019-11-10 19:07:25 +0900

    SafeAreaとUnsafeAreaの基底クラスSafeAreaBase追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/SafeAreaBase.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+
+namespace Jagapippi.AutoScreen
+{
+    [ExecuteAlways]
+    public abstract class SafeAreaBase : MonoBehaviour
+    {
+        private RectTransform _rectTransform;
+        protected RectTransform rectTransform => _rectTransform ?? (_rectTransform = this.GetComponent<RectTransform>());
+
+        void Reset()
+        {
+            this.ResetRect();
+            this.UpdateRect();
+        }
+
+        protected virtual void ResetRect()
+        {
+            this.rectTransform.sizeDelta = Vector3.zero;
+            this.rectTransform.anchoredPosition = Vector3.zero;
+            this.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            this.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+            this.rectTransform.localRotation = Quaternion.identity;
+            this.rectTransform.localScale = Vector3.one;
+        }
+
+#if UNITY_EDITOR
+        void OnEnable()
+        {
+            GameViewEvent.resolutionChanged += gameViewSize =>
+            {
+                if (this != null) this.UpdateRect();
+            };
+            EditorSceneManager.sceneSaving += (scene, path) =>
+            {
+                if (this.rectTransform) this.ResetRect();
+            };
+            EditorSceneManager.sceneSaved += scene =>
+            {
+                if (this.rectTransform) this.UpdateRect();
+            };
+
+            this.rectTransform.hideFlags = HideFlags.NotEditable;
+        }
+
+        void OnDisable() => this.rectTransform.hideFlags = HideFlags.None;
+
+        private bool _isDirty;
+
+        void OnValidate() => _isDirty = true;
+
+        void Update()
+        {
+            if (_isDirty == false) return;
+
+            _isDirty = false;
+            this.UpdateRect();
+        }
+#endif
+        void Start() => this.UpdateRect();
+
+        public void UpdateRect() => this.UpdateRect(Screen.safeArea, Screen.width, Screen.height);
+        public abstract void UpdateRect(Rect safeArea, int width, int height);
+    }
+}
--- Assets/Jagapippi/AutoScreen/Scripts/UnsafeArea.cs
+++ Assets/Jagapippi/AutoScreen/Scripts/UnsafeArea.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor.SceneManagement;
-#endif
 
 namespace Jagapippi.AutoScreen
 {
-    [ExecuteAlways]
-    public class UnsafeArea : MonoBehaviour
+    public class UnsafeArea : SafeAreaBase
     {
         public enum Position
         {
@@ -25,57 +21,7 @@ namespace Jagapippi.AutoScreen
             set { _position = value; }
         }
 
-        private RectTransform _rectTransform;
-        private RectTransform rectTransform => (_rectTransform != null) ? _rectTransform : (_rectTransform = this.GetComponent<RectTransform>());
-
-        void Reset()
-        {
-            this.ResetRect();
-            this.UpdateRect();
-        }
-
-        private void ResetRect()
-        {
-            this.rectTransform.sizeDelta = this.rectTransform.anchoredPosition = Vector3.zero;
-            this.rectTransform.anchorMin = Vector2.zero;
-            this.rectTransform.anchorMax = Vector2.zero;
-            this.rectTransform.localRotation = Quaternion.identity;
-            this.rectTransform.localScale = Vector3.one;
-        }
-
-#if UNITY_EDITOR
-        void OnEnable()
-        {
-            GameViewEvent.resolutionChanged += gameViewSize =>
-            {
-                if (this != null) this.UpdateRect();
-            };
-            EditorSceneManager.sceneSaving += (scene, path) => this.ResetRect();
-            EditorSceneManager.sceneSaved += scene => this.UpdateRect();
-
-            this.rectTransform.hideFlags = HideFlags.NotEditable;
-        }
-
-        void OnDisable() => this.rectTransform.hideFlags = HideFlags.None;
-
-        private bool _isDirty = false;
-
-        void OnValidate() => _isDirty = true;
-
-        void Update()
-        {
-            if (_isDirty)
-            {
-                _isDirty = false;
-                this.UpdateRect();
-            }
-        }
-#endif
-        void Start() => this.UpdateRect();
-
-        public void UpdateRect() => this.UpdateRect(Screen.safeArea, Screen.width, Screen.height);
-
-        public void UpdateRect(Rect safeArea, int width, int height)
+        public override void UpdateRect(Rect safeArea, int width, int height)
         {
             if ((safeArea.width == width) && (safeArea.height == height))
             {
