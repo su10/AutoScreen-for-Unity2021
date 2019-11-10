213c5d8564dc6b9e13f5fcdc0775b46495ff0e2f
su10 <1687366+su10@users.noreply.github.com>
2019-11-10 20:19:10 +0900

    端末の向きが変わったら自動的にセーフエリア対応コンポーネントを更新するコンポーネント追加

---
+++ Assets/Jagapippi/AutoScreen/Scripts/RuntimeSafeAreaUpdater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.AutoScreen
+{
+    public class RuntimeSafeAreaUpdater : MonoBehaviour
+    {
+        private ISafeAreaUpdatable _target;
+        private ScreenOrientation _orientation;
+
+        void Start()
+        {
+            _target = this.GetComponent<ISafeAreaUpdatable>();
+
+            _orientation = Screen.orientation;
+            _target.UpdateRect();
+        }
+
+        void Update()
+        {
+            if (_orientation == Screen.orientation) return;
+
+            _orientation = Screen.orientation;
+            _target.UpdateRect();
+        }
+    }
+}
