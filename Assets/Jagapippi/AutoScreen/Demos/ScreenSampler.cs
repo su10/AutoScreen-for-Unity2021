42a9eb35dec76290437d791f317545a7fd50c726
su10 <1687366+su10@users.noreply.github.com>
2019-11-12 02:50:55 +0900

    デモシーン追加

---
+++ Assets/Jagapippi/AutoScreen/Demos/ScreenSampler.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jagapippi.AutoScreen.Demos
+{
+    [ExecuteAlways]
+    public class ScreenSampler : MonoBehaviour
+    {
+        [SerializeField] private InputField _inputField = null;
+
+        void Update()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append($"Screen.size: {Screen.size}\n");
+            builder.Append($"Screen.safeArea:\n{Screen.safeArea}\n");
+            builder.Append($"Screen.orientation: {Screen.orientation}\n");
+#if UNITY_2019_2_OR_NEWER
+            builder.Append($"Screen.brightness: {Screen.brightness}\n"); // 1 in editor
+            builder.Append($"Screen.cutouts.Length: {Screen.cutouts.Length}\n");
+            builder.Append($"Screen.cutouts: \n{string.Join(",\n", Screen.cutouts.Select(c => c.ToString()).ToList())}\n");
+#endif
+
+            _inputField.text = builder.ToString();
+        }
+    }
+}
