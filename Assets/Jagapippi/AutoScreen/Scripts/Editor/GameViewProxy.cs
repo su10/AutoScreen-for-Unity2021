149aaf673cd21de5bf848ef04aa991b002d2b504
su10 <1687366+su10@users.noreply.github.com>
2020-08-10 23:30:49 +0900

    Unity2019.3以降でエラーが出続ける問題対応

--- Assets/Jagapippi/AutoScreen/Scripts/Editor/GameViewProxy.cs
+++ Assets/Jagapippi/AutoScreen/Scripts/Editor/GameViewProxy.cs
@@ -12,14 +12,13 @@ namespace Jagapippi.AutoScreen
         private const BindingFlags StaticFlag = BindingFlags.Static | BindingFlags.NonPublic;
 
         private static readonly Type GameView = Assembly.Load("UnityEditor.dll").GetType("UnityEditor.GameView");
-        private static readonly PropertyInfo HasFocus = GameView.GetProperty("hasFocus", InstanceFlag);
         private static readonly PropertyInfo CurrentSizeGroupType = GameView.GetProperty("currentSizeGroupType", StaticFlag);
         private static readonly PropertyInfo SelectedSizeIndex = GameView.GetProperty("selectedSizeIndex", InstanceFlag);
         private static readonly PropertyInfo CurrentGameViewSize = GameView.GetProperty("currentGameViewSize", InstanceFlag);
 
         internal static EditorWindow instance => EditorWindow.GetWindow(GameView, false, "Game", false);
         public static bool isOpen => (0 < Resources.FindObjectsOfTypeAll(GameView).Length);
-        public static bool hasFocus => (isOpen && (bool) HasFocus.GetValue(instance, null));
+        public static bool hasFocus => (isOpen && EditorWindow.focusedWindow && (EditorWindow.focusedWindow.GetType() == GameView));
         public static GameViewSizeGroupType currentSizeGroupType => (GameViewSizeGroupType) CurrentSizeGroupType.GetValue(instance, null);
         public static int selectedSizeIndex => (int) SelectedSizeIndex.GetValue(instance, null);
 
