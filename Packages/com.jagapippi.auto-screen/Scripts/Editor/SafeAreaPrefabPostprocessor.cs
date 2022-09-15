c4710d2158be54bb184e3cf66dca634f28795c23
su10 <1687366+su10@users.noreply.github.com>
2022-09-15 21:00:40 +0900

    プレハブ生成時にセーフエリアがリセットされない問題修正

--- Packages/com.jagapippi.auto-screen/Scripts/Editor/SafeAreaPrefabPostprocessor.cs
+++ Packages/com.jagapippi.auto-screen/Scripts/Editor/SafeAreaPrefabPostprocessor.cs
@@ -1,5 +1,4 @@
 ﻿#if UNITY_EDITOR
-using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -8,21 +7,29 @@ namespace Jagapippi.AutoScreen
 {
     class SafeAreaPrefabPostprocessor : AssetPostprocessor
     {
-        private static readonly List<string> _processedPathList = new List<string>();
-
-        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
             foreach (var path in importedAssets)
             {
                 if (Path.GetExtension(path) != ".prefab") continue;
-                if (_processedPathList.Remove(path)) continue;
 
                 var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                var isDirty = false;
 
                 foreach (var safeArea in prefab.GetComponentsInChildren<ISafeAreaUpdatable>(true))
                 {
                     safeArea.ResetRect();
-                    _processedPathList.Add(path);
+                    isDirty = true;
+                }
+
+                if (isDirty)
+                {
+                    EditorUtility.SetDirty(prefab);
+#if UNITY_2021_2_OR_NEWER
+                    AssetDatabase.SaveAssetIfDirty(prefab);
+#else
+                    AssetDatabase.SaveAssets();
+#endif
                 }
             }
         }
